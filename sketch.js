104df6c3dfaacd0b5f348714dffc1762488793d0
unknown <singh.madhu004@gmail.com>
2020-04-16 08:11:02 -0700

    plateform name

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platform1.display();
 }
